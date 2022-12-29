7fb87b99b5c42c017b16f0dd0f0c8aefffd93b13
jaejun Kim <jjkim092@gmail.com>
2022-12-29 23:51:14 +0900

    Modified   GUnit/test.cpp Modified   TDD/Dollar.h

--- GUnit/test.cpp
+++ GUnit/test.cpp
@@ -2,10 +2,61 @@
 #include "../TDD/Math.h"
 #include "../TDD/Dollar.h"
 
-TEST(testMultiplication, TestName)
+
+TEST(testMultiplication)
 {
-    Dollar five = Dollar(5);
-    five.times(2);
-    EXPECT_EQ(10, five.amount);
+    Money* five = Money::dollar(5);
+    EXPECT_EQ(*(Money::dollar(10)), *(five->times(2)));
+    EXPECT_EQ(*(Money::dollar(15)), *(five->times(3)));
+};
+
+TEST(testFrancMultiplication)
+{
+    Money* five = Money::franc(5);
+    EXPECT_EQ(*(Money::franc(10)), *(five->times(2)));
+    EXPECT_EQ(*(Money::franc(15)), *(five->times(3)));
+};
+
+TEST(testEquality)
+{
+    EXPECT_TRUE(*(Money::dollar(5)) == *(Money::dollar(5)));
+    EXPECT_FALSE(*(Money::dollar(5)) == *(Money::dollar(6)));
+    EXPECT_TRUE(*(Money::franc(5)) == *(Money::franc(5)));
+    EXPECT_FALSE(*(Money::franc(5)) == *(Money::franc(6)));
+    EXPECT_FALSE(*(Money::dollar(5)) == *(Money::franc(5)));
 }
 
+TEST(testCurrency)
+{
+    EXPECT_EQ(std::string("CHF"), "CHF");
+    EXPECT_EQ(std::string("USD"), "USD");
+}
+
+TEST(testDifferenctClassEquality)
+{
+//     EXPECT_EQ(*(new Money(10, "CHF")), *(new Franc(10, "CHF")));
+}
+
+TEST(testSimpleAdditiion)
+{
+    Money* five = Money::franc(5);
+    IExpression* result = *five + *five;
+    Sum* sum = static_cast<Sum*>(result);
+    EXPECT_EQ(*five, sum->augend);
+    EXPECT_EQ(*five, sum->augend);
+}
+
+TEST(testReduceSum)
+{
+    IExpression* sum = new Sum(*Money::dollar(3), *Money::dollar(4));
+    Bank bank;
+    Money* result = bank.reduce(*sum, "USD");
+    EXPECT_EQ(*Money::dollar(7), *result);
+}
+
+TEST(testReduceMoney)
+{
+    Bank* bank = new Bank();
+    Money* result = bank->reduce(*Money::dollar(1), "USD");
+    EXPECT_EQ(*Money::dollar(1), *result);
+}
--- TDD/Dollar.h
+++ TDD/Dollar.h
@@ -1,27 +1,140 @@
 #pragma once
-class Dollar
+
+#include <typeinfo>
+#include <Windows.h>
+#include <tchar.h>
+#include <debugapi.h>
+#include <string>
+
+class Money;
+
+interface IExpression
+{
+    virtual Money* reduce(std::string to) = 0;
+};
+
+class Money : public IExpression
 {
 public:
-    explicit Dollar(int amount)
+    explicit Money(int amount, std::string currency)
     {
         this->amount = amount;
-    };
-    
-    Dollar* times(int multiplier) 
-    {
-        return new Dollar(amount * multiplier);
-    };
-
-    bool equals(Dollar* object)
-    {
-        return amount == object->amount;
+        this->currency = currency;
     }
 
-    friend bool operator==(const Dollar& lhs, const Dollar& rhs)
+
+    Money* reduce(std::string to) override
     {
-        return lhs.amount == rhs.amount;
+        return this;
     }
 
-private:
+public:
     int amount;
+
+
+public:
+    friend bool operator==(const Money& lhs, const Money& rhs)
+    {
+        return (lhs.amount == rhs.amount) && (lhs.getCurrency() == rhs.getCurrency());
+    }
+
+//     friend IExpression operator+(const Money& lhs, const Money& rhs);
+
+public:
+    virtual const type_info& getClass() const
+    {
+        return typeid(*this);
+    }
+
+    static Money* dollar(int amount);
+    static Money* franc(int amount);
+
+    Money* times(int multiplier)
+    {
+        return new Money(amount * multiplier, currency);
+    }
+
+    std::string getCurrency() const
+    {
+        return currency;
+    }
+
+protected:
+    std::string currency;
 };
+
+// class Dollar : public Money
+// {
+// public:
+//     explicit Dollar(int amount, std::string InCurrency)
+//         : Money(amount, InCurrency)
+//     {
+//     };
+// 
+//     bool equals(Dollar* object)
+//     {
+//         return amount == object->amount;
+//     }
+// };
+
+Money* Money::dollar(int amount)
+{
+    return new Money(amount, "USD");
+}
+
+// class Franc : public Money
+// {
+// public:
+//     explicit Franc(int amount, std::string InCurrency)
+//         : Money(amount, InCurrency)
+//     {
+//     };
+// 
+//     bool equals(Franc* object)
+//     {
+//         return amount == object->amount;
+//     }
+// };
+
+
+Money* Money::franc(int amount)
+{
+    return new Money(amount, "CHF");
+}
+
+class Bank
+{
+public:
+    Money* reduce(IExpression& source, std::string to);
+};
+
+class Sum : public IExpression
+{
+public:
+    Sum(const Money& InAugend, const Money& InAddend)
+        : augend(InAugend), addend(InAddend)
+    {
+
+    }
+
+    Money* reduce(std::string to)
+    {
+        int amount = augend.amount + addend.amount;
+        return new Money(amount, to);
+    }
+
+    const Money& augend;
+    const Money& addend;
+};
+
+Money* Bank::reduce(IExpression& source, std::string to)
+{
+    return source.reduce(to);
+}
+
+IExpression* operator+(const Money& lhs, const Money& rhs)
+{
+    IExpression* added = new Sum(lhs, rhs);
+    return added;
+}
+
